616e651f1e5bbbb1a6aab0480ab2410bf9770c61
Aditya Dhikale <113303994+adityadhikale@users.noreply.github.com>
2023-03-13 21:25:25 +0530

    adding artist
    adding artist name in current playing song

--- js/wlidtrack.js
+++ js/wlidtrack.js
@@ -2,12 +2,12 @@ console.log("Welcome to WildTrack");
 
 // list of songs name , file path and poster
 let songs = [
-    {songName :'Yellow', filePath : 'songs/1.mp3', coverPath : 'https://i.scdn.co/image/ab67616d0000b2733d92b2ad5af9fbc8637425f0' },
-    {songName :'Photograph', filePath : 'songs/2.mp3', coverPath : 'https://i.scdn.co/image/ab67616d00001e02407981084d79d283e24d428e' },
-    {songName :'Night Changes', filePath : 'songs/3.mp3', coverPath : 'https://i.scdn.co/image/ab67616d0000b273d304ba2d71de306812eebaf4' },
-    {songName :'Something Just Like This', filePath : 'songs/4.mp3', coverPath : 'https://i.scdn.co/image/ab67616d0000b273198a167382d64e10284be9ec' },
+    {songName :'Yellow', filePath : 'songs/1.mp3', coverPath : 'https://i.scdn.co/image/ab67616d0000b2733d92b2ad5af9fbc8637425f0' ,artist: 'Coldplay' },
+    {songName :'Photograph', filePath : 'songs/2.mp3', coverPath : 'https://i.scdn.co/image/ab67616d00001e02407981084d79d283e24d428e' ,artist:'Ed Sheeran'},
+    {songName :'Night Changes', filePath : 'songs/3.mp3', coverPath : 'https://i.scdn.co/image/ab67616d0000b273d304ba2d71de306812eebaf4', artist:'One Direction'},
+    {songName :'Something Just Like This', filePath : 'songs/4.mp3', coverPath : 'https://i.scdn.co/image/ab67616d0000b273198a167382d64e10284be9ec',artist:'Coldplay and The Chainsmokers' },
     {songName :'Memories', filePath : 'songs/5.mp3', coverPath : 'https://i.scdn.co/image/ab67616d0000b273c1fda32d73010571ddfc1f7e' },
-    {songName :'You Are The Reason', filePath : 'songs/6.mp3', coverPath : 'https://i.scdn.co/image/ab67616d0000b273f2d671c22b70e01b78a618a8' }
+    {songName :'You Are The Reason', filePath : 'songs/6.mp3', coverPath : 'https://i.scdn.co/image/ab67616d0000b273f2d671c22b70e01b78a618a8',artist:'Maroon 5' }
 ]
 
 //initilizing the variables
@@ -16,6 +16,7 @@ var play_img = document.getElementById("play_img")
 let progressbar = document.getElementById("progressbar");
 let audioElement = new Audio('songs/1.mp3');
 var current_name = document.getElementById("current_name");
+var current_artist = document.getElementById("current_artist");
 var current_poster = document.getElementById("current_poster");
 let songIndex = 0;
 
@@ -55,6 +56,7 @@ document.getElementById('next').addEventListener('click',()=>{
     audioElement.src = songs[songIndex].filePath;
     audioElement.currentTime = 0;
     current_name.innerHTML = songs[songIndex].songName;
+    current_artist.innerHTML = songs[songIndex].artist;
     current_poster.src = songs[songIndex].coverPath;
     audioElement.play();
     console.log(audioElement.src);
@@ -74,6 +76,7 @@ document.getElementById("previous").addEventListener('click',()=>{
     audioElement.src = songs[songIndex].filePath;
     audioElement.currentTime = 0;
     current_name.innerHTML = songs[songIndex].songName;
+    current_artist.innerHTML = songs[songIndex].artist;
     current_poster.src = songs[songIndex].coverPath;
     audioElement.play();
     console.log(audioElement.src);
